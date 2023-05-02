9035d689e7c677b38197724584969020766c3213
VladRidun <ridunvv@gmail.com>
2023-05-02 09:58:37 +0400

    3. Состояние нити. 3

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -11,7 +11,9 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("job is done");
         }
-        System.out.println("Jobs is done");
+        System.out.println("First Thread -" + first.getState());
+        System.out.println("Second Thread -" + second.getState());
     }
 }
