8f65382e367a0f49e09de6f2dff2fd6659b9c62c
VladRidun <ridunvv@gmail.com>
2023-05-05 18:58:56 +0400

    1. Синхронизация общих ресурсов.2

--- src/main/java/ru/job4j/concurrent/Cache.java
+++ src/main/java/ru/job4j/concurrent/Cache.java
@@ -3,7 +3,7 @@ package ru.job4j.concurrent;
 public final class Cache {
     private static Cache cache;
 
-    public static Cache instOf() {
+    public Cache instOf() {
         synchronized (this) {
             if (cache == null) {
                 cache = new Cache();
