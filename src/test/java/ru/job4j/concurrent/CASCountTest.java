ec94d9211da7acc8155d604f88dfcd2e92705476
VladRidun <ridunvv@gmail.com>
2023-06-05 13:46:18 +0400

    0. CAS - операции [#6859 # [#6859 #420935]]

---
+++ src/test/java/ru/job4j/concurrent/CASCountTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CasCountTest {
+
+    @Test
+    void whenExecute2ThreadThen20() throws InterruptedException {
+        var count = new CASCount();
+        var first = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                count.increment();
+            }
+        });
+        var second = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                count.increment();
+            }
+        });
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get()).isEqualTo(10);
+    }
+}
