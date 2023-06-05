e3ea03e6a77561096b51d5a672d5b8f8722b8b4b
VladRidun <ridunvv@gmail.com>
2023-06-05 13:10:31 +0400

    0. CAS - операции [#6859 # [#6859]]

---
+++ src/main/java/ru/job4j/concurrent/CASCount.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicInteger count = new AtomicInteger(0);
+
+    public void increment() {
+        int expectedValue;
+        int newValue;
+        do {
+            expectedValue = count.get();
+            newValue = expectedValue + 1;
+        } while (!count.compareAndSet(expectedValue, newValue));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
