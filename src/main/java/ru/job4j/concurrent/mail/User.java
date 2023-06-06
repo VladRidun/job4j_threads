40c64abfca0f5366d9cb5436b91e638bc753dfa2
VladRidun <ridunvv@gmail.com>
2023-06-06 16:41:26 +0400

    2. ExecutorService рассылка почты. [#63097 # [#63097]]

---
+++ src/main/java/ru/job4j/concurrent/mail/User.java
@@ -0,0 +1,14 @@
+package ru.job4j.concurrent.mail;
+
+public class User {
+    private String username;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
